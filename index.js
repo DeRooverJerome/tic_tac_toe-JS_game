0590fd30433fe6ef02f9f464e54877b911a5ee51
DeRooverJerome <derooverjdr@gmail.com>
2023-10-06 08:13:03 +0200

    final

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const winCombinations = [
 let board = document.querySelector(".board");
 let playedCells = Array(9).fill("");
 let current_Player = "circle";
-let restartButton = document.querySelector(".restart");
+let restartButton = document
+  .querySelector(".restart")
+  .addEventListener("click", restartGame);
 let gameStillRun = true;
 let message = document.querySelector(".message");
 
@@ -33,6 +35,9 @@ createBoard();
 // Evenement au clic pour ajouter une div de classe circle ou cross
 
 function addPone(e) {
+  if (!gameStillRun) {
+    return;
+  }
   const PoneDisplay = document.createElement("div");
   PoneDisplay.classList.add(current_Player);
   e.target.append(PoneDisplay);
@@ -46,6 +51,7 @@ function addPone(e) {
 // Condition de victoire
 
 function checkWin() {
+  let isDraw = true;
   for (const combination of winCombinations) {
     const [a, b, c] = combination;
     if (
@@ -58,5 +64,29 @@ function checkWin() {
       return;
     }
   }
+  for (const cell of playedCells) {
+    if (cell === "") {
+      isDraw = false;
+      return;
+    }
+  }
+  if (isDraw) {
+    message.textContent = `It's a draw !`;
+    gameStillRun = false;
+  }
 }
 
+// On restart la game
+
+function restartGame() {
+  playedCells = Array(9).fill("");
+  current_Player = "circle";
+  gameStillRun = true;
+  message.textContent = `Welcome, ${current_Player} begin`;
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.innerHTML = "";
+    cell.addEventListener("click", addPone);
+  });
+}
+restartGame();
